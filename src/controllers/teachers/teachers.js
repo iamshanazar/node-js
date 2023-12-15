c1ed5d0e0fdce3838598c31f480869819c2b9cde
iamshanazar <ahmedowshanazar@gmail.com>
2023-12-15 18:07:55 +0500

    join added in teachers controller

--- src/controllers/teachers/teachers.js
+++ src/controllers/teachers/teachers.js
@@ -3,26 +3,27 @@ import {execute} from '../../../database/db.js'
 import bcrypt from 'bcrypt'
 
 export default class teachersController {
+
   static async getTeachers(req, res) { 
   const id = req.query.department_id
   let limit = req.query.limit;
   let page = req.query.page;
-   
+ 
+
   if (!req.query.limit && !req.query.page) {
     limit = 10;
     page = 1;
   }
-
+   
   const offset = (page - 1) * limit;
-
     try {
       const { rows } = await execute(
-        `SELECT * FROM teachers  WHERE department_id=$1  ORDER BY created_at DESC LIMIT ${limit} OFFSET ${offset}`,
-        [id,]
+        `SELECT * FROM teachers t INNER JOIN departments d ON t.department_id = d.id  WHERE department_id = $1 LIMIT ${limit} OFFSET ${offset}`,
+        [id]
       );
       if (rows) {
         res.send({
-          data: rows,
+          data: rows, 
           status: true,
         });
       } else {
@@ -43,7 +44,7 @@ export default class teachersController {
 
     const {firstname, lastname, name, password,  department_id, sirname, job } = req.body;
     const pass =  await bcrypt.hash(password, 10)
-    try {
+  try {
       const result = await execute(
         `INSERT INTO teachers (firstname, lastname, department_id, sirname, job) VALUES ($1,$2,$3,$4, $5 )`,
         [firstname, lastname, department_id, sirname, job]
--- src/routers/middleware.js
+++ src/routers/middleware.js
@@ -2,13 +2,13 @@ import jwt from 'jsonwebtoken'
 
 export const authenticate = async (req, res, next) => {
   const auth = req.headers?.authorization
+
   if (!auth) {
     res.status(401).send('Missing authorization header')
-  }
-  
-  try {
-    // const token = auth.split(' ')[1]
-    const decodedToken = jwt.verify(auth, process.env.ACCESS_TOKEN_SECRET_KEY)
+  } 
+  try { 
+    const token = auth.split(' ')[1]
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY)
     const userId = decodedToken.userId
     if (req.body.userId !== userId) {
       res.status(401).send('Invalid authorization header')
