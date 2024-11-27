ca6cd2b3595b4430405ff32a85d4c0a03312b6a3
iamshanazar <ahmedowshanazar@gmail.com>
2024-11-27 23:42:31 +0500

    fixed middleware

--- src/routers/middleware.js
+++ src/routers/middleware.js
@@ -3,23 +3,21 @@ import jwt from 'jsonwebtoken'
 export const authenticate = async (req, res, next) => {
   const auth = req.headers?.authorization
 
-  console.log(auth,'auth')
-  console.log(req.headers)
 
   if (!auth) {
-    return res.status(401).send('Missing authorization header')
+     res.status(401).send('Missing authorization header')
   } 
   try { 
     const token = auth.split(' ')[1]
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY)
     const userId = decodedToken.userId
     if (req.body.userId !== userId) {
-       return res.status(401).send('Invalid authorization header')
+        res.status(401).send('Invalid authorization header')
     } else {
       next()
     }
   } catch (err) {
-    return res.status(403).json({
+     res.status(403).json({
       error: new Error('Invalid request!'),
       message: err.message,
     })
